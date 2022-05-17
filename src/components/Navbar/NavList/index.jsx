7e187e791df32d320f77632fb7446a9f30da2852
Asliddin <asliddinabijonov04@gmail.com>
2022-05-17 05:09:49 -0700

    footer bn navbarni boldim

--- src/components/Navbar/NavList/index.jsx
+++ src/components/Navbar/NavList/index.jsx
@@ -5,7 +5,11 @@ export default ({ flex, none }) => {
   return (
     <>
       {flex && (
-        <List component="div" disablePadding>
+        <List
+          component="div"
+          disablePadding
+          sx={{ backgroundColor: "#4C4C4C" }}
+        >
           {["Home", "Find a jobs", "About", "Page+", "Contact"].map((item) => (
             <ListItemButton key={item}>
               <Typography sx={{ color: "white", fontSize: 15 }}>
@@ -16,10 +20,35 @@ export default ({ flex, none }) => {
         </List>
       )}
       {none && (
-        <List component="div" disablePadding sx={{ display: "flex" }}>
-          {["Home", "Find a jobs", "About", "Page+", "Contact"].map((item) => (
-            <ListItemButton key={item}>
-              <Typography sx={{ color: "black", fontSize: 15 }}>
+        <List
+          component="div"
+          disablePadding
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {["Home", "Find a jobs", "About", "Page", "Contact"].map((item) => (
+            <ListItemButton
+              key={item}
+              sx={{
+                maxWidth: "30%",
+                maxHeight: "20px",
+                ":hover": {
+                  backgroundColor: "white",
+                },
+              }}
+            >
+              <Typography
+                sx={{
+                  color: "black",
+                  fontSize: 15,
+                  ":hover": {
+                    color: "#FB246A",
+                  },
+                }}
+              >
                 {item}
               </Typography>
             </ListItemButton>
